3f0ff661a317b16875bf5f50dd5b1b265f31a315
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-09 14:26:15 +0100

    Imported font

--- web/index.php
+++ web/index.php
@@ -12,13 +12,26 @@
     <!-- <link rel="icon" type="image/x-icon" href="" />/ -->
     <title>Currency Converter</title>
 
-    <!-- font library -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Jost:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
     <!-- icon library -->
     
     <link rel="stylesheet" type="text/css" href="./css/styles.css" />
     <script type="text/javascript" src="./js/script.js"></script>
 </head>
 <body>
-    
+    <header>
+        <h1>Currency Converter</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
 </html>
