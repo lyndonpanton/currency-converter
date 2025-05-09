1bd0b5a3b845c004f06bcd6d89dcab86326e75ef
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-09 14:23:17 +0100

    Defined metadata

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en-GB">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="application-name" content="Currency Converter" />
+    <meta name="author" content="Lyndon Mykal Panton" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,
+            user-scalable=true, minimum-scale=0.25, maximum-scale=4.0" />
+
+    <!-- <link rel="icon" type="image/x-icon" href="" />/ -->
+    <title>Currency Converter</title>
+
+    <!-- font library -->
+    <!-- icon library -->
+    
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <script type="text/javascript" src="./js/script.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
