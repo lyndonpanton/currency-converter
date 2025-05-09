a98ac11beabb1e818d16b794d83097e44d1ebddc
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-09 16:07:04 +0100

    Added fontawesome api

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
+<?php
+    include("config.php");
+?>
+
 <!DOCTYPE HTML>
 <html lang="en-GB">
 <head>
@@ -16,9 +20,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Jost:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
-    <!-- icon library -->
+    <script src=<?php echo "https://kit.fontawesome.com/" . API_KEY_FONTAWESOME . ".js"; ?> crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="./css/desktop.css" />
     <script type="text/javascript" src="./js/script.js"></script>
 </head>
 <body>
